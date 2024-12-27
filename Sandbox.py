f8eda0a5c08b2d1aefbe9dfc3d13c04243239d92
samkumbi <samkumbi@outlook.com>
2024-12-27 17:48:25 -0600

    PhoneBook
    Committing new project: Phonebook

---
+++ Sandbox.py
@@ -0,0 +1,19 @@
+# App name   => Sandbox 
+# Programmer => Sam Kumbi
+
+# App description
+# ---------------
+# This is where all the fun happens.
+# Run experimental code here.
+
+# Tuple
+# quotient, remainder = divmod(8, 3)
+# print("Quotient  = " + str(quotient))
+# print("Remainder = " + str(remainder))
+
+# tuple_example = ("one", "two", "three")
+
+# print(tuple_example)
+# print(tuple_example[0])
+# print(tuple_example[1])
+# print(tuple_example[2])
